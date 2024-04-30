2ca50893ab9cb19a7bba1f80fc7090ca93a26a47
internet-unable <polocatic@gmail.com>
2024-04-30 16:49:50 +0300

    Update project indent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+    return (
+        <>
+            <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+        </>
+    );
 }
 
 export default App;
