6c07b02a698ce10afba51241fc5b7329fa3848c7
internet-unable <polocatic@gmail.com>
2024-05-01 18:23:01 +0300

    Update for SelectedProject
    Added code that allow to add / delete tasks

--- src/components/SelectedProject/SelectedProject.jsx
+++ src/components/SelectedProject/SelectedProject.jsx
@@ -1,9 +1,41 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Button from "../Button/Button.jsx";
 import Input from "../Input/Input.jsx";
 
 export default function SelectedProject({ project }) {
-    const [taskList, setTaskList] = useState();
+    const taskNameValue = useRef();
+    const [taskList, setTaskList] = useState([]);
+
+    function handleDeleteBtnClick() {
+        // todo - delete project
+    }
+
+    function handleAddTaskBtnClick() {
+        if (taskNameValue.current.value) {
+            setTaskList(prevTaskList => {
+                return [
+                    ...prevTaskList,
+                    {
+                        id: Math.floor(Math.random() * 1000),
+                        taskName: taskNameValue.current.value
+                    }
+                    
+                ];
+            });
+        }
+        // todo - clear task name input, after click on btn
+    }
+
+    function handleClearTaskBtnClick(id) {
+        setTaskList(prevTaskList => {
+            let array = [...prevTaskList];
+            const itemInArrayById = array.find(item => item.id === id);
+            const indexOfItemInArray = array.indexOf(itemInArrayById);
+
+            array.splice(indexOfItemInArray, 1);
+            return array;
+        });
+    }
 
     return (
         <section className="flex flex-col justify-center items-center grow h-full overflow-x-auto">
@@ -19,9 +51,22 @@ export default function SelectedProject({ project }) {
                         id="tasks"
                         type="text"
                         placeholder="Task name"
+                        ref={taskNameValue}
                     />
-                    <button>Add task</button>
+                    <Button handleClick={handleAddTaskBtnClick}>Add task</Button>
                 </div>
+                <ul>
+                    {taskList.map(item => {
+                        return (
+                            <li key={item.id}>
+                                <div>
+                                    <p>{item.taskName}</p>
+                                    <Button handleClick={() => handleClearTaskBtnClick(item.id)}>Clear</Button>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         </section>
     );
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,6 @@
 import Button from "../Button/Button.jsx";
 
 export default function Sidebar({ handleAddBtnClick, projectsList, selectActiveProject }) {
-    console.log(`Sidebar ${projectsList}`);
     return (
         <aside className="h-full overflow-x-auto bg-stone-950 rounded-tr-2xl basis-96 text-stone-50">
             <h1 className="text-2xl">Your projects</h1>
